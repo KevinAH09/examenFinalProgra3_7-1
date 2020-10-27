452e2e695052581267b032e245d5e4f21aa21da0
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-27 11:20:10 -0600

    update dtos

--- tareas/src/main/java/org/una/examen/tareas/dtos/ProyectoDTO.java
+++ tareas/src/main/java/org/una/examen/tareas/dtos/ProyectoDTO.java
@@ -5,10 +5,6 @@
  */
 package org.una.examen.tareas.dtos;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,15 +19,11 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class ProyectoDTO {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
-    @Column(length = 45)
     private String nombre;
-    
-    @Column(length = 100)
+
     private String descripcion;
-    
+
 }
